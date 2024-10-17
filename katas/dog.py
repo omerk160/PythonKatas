86718ee7a913fd5d17d1298438b9b67dddb53537
omerk160 <omerkanias160@gmail.com>
2024-10-17 13:33:34 +0300

    17/10/24

--- katas/dog.py
+++ katas/dog.py
@@ -7,7 +7,7 @@ class Dog:
     1. In the constructor, create a new attribute called `self.position` which defines the position state of the dog.
        The attribute possible values are: sitting, standing, jumping. Default is sitting.
     2. Implement the following methods: sit(), stand(), jump(). Each method changes the position of the dog.
-    3. Extend the bark() function to receive an argument called `n` with default values of 2, the god should bark n times.
+    3. Extend the bark() function to receive an argument called `n` with default values of 2, the dog should bark n times.
     4. Add the dog position to the description printed in describe method. E.g.: `I'm John, a Puddle. I'm standing.`
 
     """
@@ -26,23 +26,23 @@ class Dog:
         self.position = "jumping"
 
     def bark(self, n=2):
-            print("Woof! " * n)
+        print("\n".join(["Woof! Woof!"] * n))
 
     def describe(self):
-        print(f"I'm {self.name}, a {self.breed}. I'm {self.position}.")
+        print(f"I'm {self.name}, a {self.breed}. I'm {self.position}", end="")
 
 
 if __name__ == "__main__":
     # Create a dog with the default position as "sitting"
     my_dog = Dog("Buddy", "Golden Retriever")
 
-    my_dog.bark()  # Woof! Woof!
-    my_dog.describe()  # I'm Buddy, a Golden Retriever. I'm sitting.
+    my_dog.bark()  # Woof! Woof! (on two lines)
+    my_dog.describe()  # I'm Buddy, a Golden Retriever. I'm sitting
 
     # Make the dog stand and describe again
     my_dog.stand()
-    my_dog.describe()  # I'm Buddy, a Golden Retriever. I'm standing.
+    my_dog.describe()  # I'm Buddy, a Golden Retriever. I'm standing
 
     # Make the dog jump and describe again
     my_dog.jump()
-    my_dog.describe()  # I'm Buddy, a Golden Retriever. I'm jumping.
+    my_dog.describe()  # I'm Buddy, a Golden Retriever. I'm jumping
